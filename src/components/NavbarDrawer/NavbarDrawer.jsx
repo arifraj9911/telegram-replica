6164cd7678be0fb758be1c63d37243399df27c4e
Arif Hossain <arif.raj9911@gmail.com>
2024-07-06 22:15:43 +0600

    user profile drawer added

---
+++ src/components/NavbarDrawer/NavbarDrawer.jsx
@@ -0,0 +1,87 @@
+import { CiBookmark } from "react-icons/ci";
+import { FaRegCircleUser } from "react-icons/fa6";
+import { GrAnnounce } from "react-icons/gr";
+import {
+  IoCallOutline,
+  IoMoonOutline,
+  IoSettingsOutline,
+} from "react-icons/io5";
+import { LuUsers } from "react-icons/lu";
+import { Link } from "react-router-dom";
+
+const NavbarDrawer = () => {
+  return (
+    <div className="drawer">
+      <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-side z-10">
+        <label
+          htmlFor="my-drawer"
+          aria-label="close sidebar"
+          className="drawer-overlay"
+        ></label>
+        <ul className="menu bg-white text-base-content min-h-full w-72 ">
+          {/* Sidebar content here */}
+          <div className="p-5">
+            <div className="avatar mb-4">
+              <div className="w-12 rounded-full">
+                <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+              </div>
+            </div>
+            <h4 className="text-[16px] font-semibold">Arif Hossain</h4>
+            <p className="text-blue-400">Set Emoji Status</p>
+          </div>
+          <hr className=" mb-3" />
+          <li>
+            <Link className="px-5 rounded-none gap-5 font-semibold" to={"/"}>
+              <LuUsers className="text-xl" />
+              New Group
+            </Link>
+          </li>
+          <li>
+            <Link className="px-5 rounded-none gap-5 font-semibold " to={"/"}>
+              <GrAnnounce className="text-xl" />
+              New Channels
+            </Link>
+          </li>
+          <li>
+            <Link className="px-5 rounded-none gap-5 font-semibold " to={"/"}>
+              <FaRegCircleUser className="text-xl" />
+              Contacts
+            </Link>
+          </li>
+          <li>
+            <Link className="px-5 rounded-none gap-5 font-semibold " to={"/"}>
+              <IoCallOutline className="text-xl" />
+              Calls
+            </Link>
+          </li>
+          <li>
+            <Link className="px-5 rounded-none gap-5 font-semibold " to={"/"}>
+              <CiBookmark className="text-xl" />
+              Saved Messages
+            </Link>
+          </li>
+          <li>
+            <Link className="px-5 rounded-none gap-5 font-semibold " to={"/"}>
+              <IoSettingsOutline className="text-xl" />
+              Settings
+            </Link>
+          </li>
+          <div className="flex items-center justify-between">
+            <li className="">
+              <Link className="px-5 rounded-none gap-5 font-semibold " to={"/"}>
+                <IoMoonOutline className="text-xl" />
+                Night Mode
+              </Link>
+            </li>
+            <span>
+              <input type="checkbox" className="toggle " defaultChecked />
+            </span>
+          </div>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default NavbarDrawer;
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,37 +1,54 @@
 /* eslint-disable react/prop-types */
 // import { useEffect, useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
+import NavbarDrawer from "../NavbarDrawer/NavbarDrawer";
 
-const Sidebar = ({chatData,handleMessage}) => {
-  
+const Sidebar = ({ chatData, handleMessage }) => {
   return (
-    <div className="w-[35%]  relative bg-white px-5 py-2">
-      <div className="flex sticky bg-white top-0 items-center gap-6">
-        <GiHamburgerMenu className="text-[22px] text-gray-400" />
+    <div className="w-[35%]  relative bg-white px-4 py-2">
+      <div className="flex sticky bg-white top-0 items-center gap-4">
+        <div className="drawer-content">
+          <label htmlFor="my-drawer" className=" drawer-button">
+            <GiHamburgerMenu className="text-[22px] text-gray-400 cursor-pointer" />
+          </label>
+        </div>
+
         <input
           type="text"
           className="border w-full py-1 px-5 outline-none rounded-2xl bg-[#F1F1F1]"
           placeholder="Search..."
         />
       </div>
-      <div className="max-h-screen mt-10 overflow-y-auto space-y-6">
+
+      <div className="max-h-screen mt-10 overflow-y-auto space-y-6 ">
         {chatData?.map((chat) => (
-          <div onClick={()=>handleMessage(chat)} key={chat.id} className="flex gap-4 cursor-pointer">
-            <div className="avatar">
-              <div className="w-16 rounded-full">
+          <div
+            onClick={() => handleMessage(chat)}
+            key={chat.id}
+            className="flex items-center gap-4 cursor-pointer "
+          >
+            <div className="avatar z-0">
+              <div className="w-14 rounded-full">
                 <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
               </div>
             </div>
-            <div className="flex justify-between  w-full">
+            <div className="flex justify-between rounded-md hover:bg-[#F1F1F1] p-2 w-full">
               <div>
                 <h4 className="text-lg font-bold">{chat?.sender?.name}</h4>
-                <p className="text-sm">{chat?.message}</p>
+                <p className="text-sm">
+                  {chat.message.length < 50 ? (
+                    <span>{chat?.message}</span>
+                  ) : (
+                    <span>{chat?.message.slice(0, 50)}...</span>
+                  )}
+                </p>
               </div>
               <div className="text-gray-600 text-[14px] mt-1">Fri</div>
             </div>
           </div>
         ))}
       </div>
+      <NavbarDrawer></NavbarDrawer>
     </div>
   );
 };
