2213f7440e4ce1f6182d8ed537a00611700db252
Arif Hossain <arif.raj9911@gmail.com>
2024-07-06 17:38:33 +0600

    message input box design added

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,55 +10,10 @@ const About = () => {
       ideas. Continually disseminate tactical initiatives before virtual
       initiatives. Appropriately target quality systems before installed base
       paradigms. Collaboratively innovate wireless methods of empowerment rather
-      than open-source leadership. Conveniently foster strategic infrastructures
+      than open-source lees. Competently integrate
+      transprategic infrastructures
       rather than cross-platform opportunities. Competently integrate
-      transparent channels rather than alternative.
-      Appropriately target quality systems before installed base paradigms.
-      Collaboratively innovate wireless metho benefits for diverse expertise.
-      Compellingly brand customer directed benefits and enabled web services.
-      Conveniently formulate standardized e-markets without highly efficient
-      infrastructures. Conveniently mesh scalable outsourcing via empowered
-      ideas. Continually disseminate tactical initiatives before virtual
-      initiatives. Appropriately target quality systems before installed base
-      paradigms. Collaboratively innovate wireless methods of empowerment rather
-      than open-source leadership. Conveniently foster strategic infrastructures
-      rather than cross-platform opportunities. Competently integrate
-      transparent channels rather than alternative.
-      Appropriately target quality systems before installed base paradigms.
-      Collaboratively innovate wireless metho benefits for diverse expertise.
-      Compellingly brand customer directed benefits and enabled web services.
-      Conveniently formulate standardized e-markets without highly efficient
-      infrastructures. Conveniently mesh scalable outsourcing via empowered
-      ideas. Continually disseminate tactical initiatives before virtual
-      initiatives. Appropriately target quality systems before installed base
-      paradigms. Collaboratively innovate wireless methods of empowerment rather
-      than open-source leadership. Conveniently foster strategic infrastructures
-      rather than cross-platform opportunities. Competently integrate
-      transparent channels rather than alternative.
-      Appropriately target quality systems before installed base paradigms.
-      Collaboratively innovate wireless metho benefits for diverse expertise.
-      Compellingly brand customer directed benefits and enabled web services.
-      Conveniently formulate standardized e-markets without highly efficient
-      infrastructures. Conveniently mesh scalable outsourcing via empowered
-      ideas. Continually disseminate tactical initiatives before virtual
-      initiatives. Appropriately target quality systems before installed base
-      paradigms. Collaboratively innovate wireless methods of empowerment rather
-      than open-source leadership. Conveniently foster strategic infrastructures
-      rather than cross-platform opportunities. Competently integrate
-      transparent channels rather than alternative.
-      Appropriately target quality systems before installed base paradigms.
-      Collaboratively innovate wireless metho benefits for diverse expertise.
-      Compellingly brand customer directed benefits and enabled web services.
-      Conveniently formulate standardized e-markets without highly efficient
-      infrastructures. Conveniently mesh scalable outsourcing via empowered
-      ideas. Continually disseminate tactical initiatives before virtual
-      initiatives. Appropriately target quality systems before installed base
-      paradigms. Collaboratively innovate wireless methods of empowerment rather
-      than open-source leadership. Conveniently foster strategic infrastructures
-      rather than cross-platform opportunities. Competently integrate
-      transparent channels rather than alternative.
-      Appropriately target quality systems before installed base paradigms.
-      Collaboratively innovate wireless metho benefits for diverse expertise.
+      transparent chabenefits for diverse expertise.
       Compellingly brand customer directed benefits and enabled web services.
       Conveniently formulate standardized e-markets without highly efficient
       infrastructures. Conveniently mesh scalable outsourcing via empowered
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,3 +1,8 @@
+import { GrAttachment } from "react-icons/gr";
+import { HiDotsVertical } from "react-icons/hi";
+import { IoSearchOutline } from "react-icons/io5";
+import { MdCall } from "react-icons/md";
+import { PiBookOpenUser } from "react-icons/pi";
 import { Link, Outlet } from "react-router-dom";
 
 const Main = () => {
@@ -12,9 +17,22 @@ const Main = () => {
         </li>
       </div>
 
-      <div className="flex-1">
-        <div className="fixed border border-gray-400 w-full bg-white p-3  top-0">
-          <h2>Name</h2>
+      <div className="flex-1 relative">
+        <div className="fixed border border-t-0 border-b-0  border-gray-300 w-full bg-white px-2  py-1 top-0">
+          <div className="flex">
+            <div>
+              <h2 className="text-[16px] font-semibold">Name</h2>
+              <p className="text-sm text-gray-400 font-normal">
+                last seen 12/12/24
+              </p>
+            </div>
+            <div className="flex items-center  gap-4 absolute left-1/2 ml-16 top-4 ">
+              <IoSearchOutline className="text-xl" />
+              <MdCall className="text-xl" />
+              <PiBookOpenUser className="text-xl" />
+              <HiDotsVertical className="text-xl" />
+            </div>
+          </div>
         </div>
         <div
           style={{
@@ -24,16 +42,17 @@ const Main = () => {
             backgroundPosition: "center",
             backgroundSize: "cover",
           }}
-          className=" w-full flex-grow flex flex-col-reverse max-h-full overflow-y-scroll px-6 py-20 "
+          className=" w-full flex-grow flex flex-col-reverse max-h-full overflow-y-scroll px-6 py-16 "
         >
           <Outlet></Outlet>
         </div>
         <div className="fixed w-full bg-white   bottom-0">
+          <GrAttachment className="absolute top-[14px] left-2 text-gray-400 text-xl" />
           <input
             type="text"
             name=""
-            placeholder="text..."
-            className="border border-green-500 p-3 w-full"
+            placeholder="Write a message..."
+            className="border border-gray-300 border-t-0 px-10 py-[10px] outline-none w-full"
             id=""
           />
         </div>
